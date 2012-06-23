bb059d3221f85034267015ae17288f76cda9bce1
Gilles Dubreuil <gilles@redhat.com>
2012-06-23 19:42:38 +1000

    Proper Licence header and GPLv3 file

--- satops.rb
+++ satops.rb
@@ -1,11 +1,26 @@
 #!/usr/bin/env ruby
-
-=begin
+=begin header
    * Name: satops
    * Description: RHN Satellite API Operator
    * Author: Gilles Dubreuil <gilles@redhat.com>
    * Date: 22 Jun 2012 
-   * Version: 1.3.2
+   * Version: 1.3.3
+   * License: Copyright 2011, 2012 Gilles Dubreuil 
+
+       This file is part of SatOps.
+
+       SatOps is free software: you can redistribute it and/or modify
+       it under the terms of the GNU General Public License as published by
+       the Free Software Foundation, either version 3 of the License, or
+       (at your option) any later version.
+ 
+       SatOps is distributed in the hope that it will be useful,
+       but WITHOUT ANY WARRANTY; without even the implied warranty of
+       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+       GNU General Public License for more details.
+
+       You should have received a copy of the GNU General Public License
+       along with SatOps.  If not, see <http://www.gnu.org/licenses/>.
 =end
 
 =begin rdoc
